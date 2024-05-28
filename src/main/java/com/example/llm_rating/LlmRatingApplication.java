7a558303fbf5f0093f4334d5b138584054e5b1d6
Tianrui Zhang (Gary) <tianrui.zhang.2101@stu.moonshotacademy.cn>
2024-05-28 18:13:53 +0800

    intial project

---
+++ src/main/java/com/example/llm_rating/LlmRatingApplication.java
@@ -0,0 +1,13 @@
+package com.example.llm_rating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmRatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmRatingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/llm_rating/LlmRatingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.llm_rating;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmRatingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
